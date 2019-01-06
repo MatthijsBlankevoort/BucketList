4b80b6a5587923374efed527302320579ba72897
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2019-01-06 22:30:48 +0100

    feature: add recycler view with checkbox layut

--- app/src/main/java/com/example/matthijsblankevoort/bucketlist/MainActivity.java
+++ app/src/main/java/com/example/matthijsblankevoort/bucketlist/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.matthijsblankevoort.bucketlist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private List<BucketListItem> bucketListItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+
+        mLayoutManager = new LinearLayoutManager(MainActivity.this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        
+        mAdapter = new BucketListAdapter(bucketListItems);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
