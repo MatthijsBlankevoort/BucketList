770fe7011b33087047a2cb78b59f771f90fd0db1
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2019-01-06 22:52:04 +0100

    feature: add create bucket list item activity

--- app/src/main/java/com/example/matthijsblankevoort/bucketlist/MainActivity.java
+++ app/src/main/java/com/example/matthijsblankevoort/bucketlist/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.matthijsblankevoort.bucketlist;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private FloatingActionButton floatingActionButton;
 
     private List<BucketListItem> bucketListItems = new ArrayList<>();
 
@@ -22,11 +26,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mRecyclerView = findViewById(R.id.recyclerView);
+        floatingActionButton = findViewById(R.id.floatingActionButton);
 
         mLayoutManager = new LinearLayoutManager(MainActivity.this);
         mRecyclerView.setLayoutManager(mLayoutManager);
-        
+
         mAdapter = new BucketListAdapter(bucketListItems);
         mRecyclerView.setAdapter(mAdapter);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CreateBucketListItemActivity.class));
+            }
+        });
     }
 }
